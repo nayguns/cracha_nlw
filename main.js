9525ddbc66c1e43c4ab2b23c0f0b972baded54b3
Gustavo Lagares <72045537+nayguns@users.noreply.github.com>
2021-11-05 22:27:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'nayguns',
+  youtube: 'nayguns',
+  facebook: 'gustavo.bueno.17',
+  twitter: 'gulagares',
+  instagram: 'gus_lagares'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
